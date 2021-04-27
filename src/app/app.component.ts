72dbe6436f9142de4d733ae57521ddbc69ee5c81
Frank Uzoka <Frankuzoka@hotmail.com>
2021-04-27 00:11:10 -0400

    First Angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-project';
+  title = 'Angular: Getting Started';
 }
